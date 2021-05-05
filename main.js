fa6e47f6c3808c0d09be4e0fb4be51e9713b9f60
lizclaro23 <83023850+lizclaro23@users.noreply.github.com>
2021-05-05 17:53:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let nomeusuario = (prompt("Qual seu nome?"))
+let pergunta = (prompt('Quer começar?' + ' 1 para Sim ' + '2 para Não'))
+
+if (pergunta == "1"){
+    document.write(nomeusuario)
+    window.alert("Vamos começar!")   
+} else {
+    window.alert("Muito obrigada!") 
+}
+
+let prompt1 = (prompt("Quanto é 2 + 2? " + "Responda 1 para 4, " + "2 para 6 ou " + "3 para 7 "))
+let prompt2 = (prompt("Quanto é 4 + 2? " + "Responda 1 para 4, " + "2 para 6 ou " + "3 para 7 "))
+let prompt3 = (prompt("Quanto é 3 + 4? " + "Responda 1 para 4, " + "2 para 6 ou " + "3 para 7 "))
